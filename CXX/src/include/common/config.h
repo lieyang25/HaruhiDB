5a976407643f56a69e0512d13a1e672257557e6f
suzumiya_haruhi <suzumiya@haruhi.com>
2026-03-04 21:03:36 +0800

    Update config.h

--- CXX/src/include/common/config.h
+++ CXX/src/include/common/config.h
@@ -12,9 +12,13 @@ namespace HaruhiDB
     constexpr size_t HEADER_SIZE = 32;
     constexpr size_t PAGE_SIZE = 4096;
 
-    using slot_id_t = uint16_t;
     using page_data_t = std::array<std::byte, PAGE_SIZE>;
     using page_id_t = uint32_t;
+    using frame_id_t = size_t;
+    using seq_t = uint64_t;
+
+    using slot_id_t = uint16_t;
     using lsn_t = uint64_t;
+    
     static constexpr page_id_t INVALID_PAGE_ID = -1;
 } // namespace HaruhiDB
