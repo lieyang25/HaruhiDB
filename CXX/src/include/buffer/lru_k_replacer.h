deb665151b4807422e16f129b214a266f7353344
suzumiya_haruhi <suzumiya@haruhi.com>
2026-03-05 13:57:10 +0800

    Update

---
+++ CXX/src/include/buffer/lru_k_replacer.h
@@ -0,0 +1,58 @@
+/**
+ * CXX/src/include/buffer/lru_k_replacer.h
+ */
+#pragma once
+
+#include "common/config.h"
+
+#include <vector>
+#include <unordered_map>
+#include <deque>
+#include <mutex>
+#include <optional>
+#include <cstdint>
+#include <limits>
+
+namespace HaruhiDB {
+namespace buffer {
+
+    class LruKReplacer
+    {
+    public:
+        explicit LruKReplacer(size_t pool_size, size_t k = 2);
+
+        void RecordAccess(frame_id_t frame_id);
+
+        void SetEvictable(frame_id_t frame_id, bool evictable);
+
+        bool Victim(frame_id_t& frame_id);
+
+        void Remove(frame_id_t frame_id);
+
+        size_t Size() const;
+
+    private:
+
+        struct FrameInfo
+        {
+            std::deque<seq_t> history;
+            bool evictable{false};
+        };
+
+    private:
+
+        mutable std::mutex latch_;
+
+        const size_t pool_size_;
+        const size_t k_;
+
+        uint64_t current_timestamp_{0};
+
+        std::vector<FrameInfo> frames_;
+
+        size_t evictable_size_{0};
+
+        frame_id_t PickVictimInternal();
+    };
+} // namespace buffer
+} // namespace HaruhiDB
