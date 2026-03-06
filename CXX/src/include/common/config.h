519748e1795951a5b1a9e96af8d1c323e4427ae8
suzumiya_haruhi <suzumiya@haruhi.com>
2026-03-06 19:35:13 +0800

    Update config.h :page_id_t->uint32_t to int32_t

--- CXX/src/include/common/config.h
+++ CXX/src/include/common/config.h
@@ -13,7 +13,7 @@ namespace HaruhiDB
     constexpr size_t PAGE_SIZE = 4096;
 
     using page_data_t = std::array<std::byte, PAGE_SIZE>;
-    using page_id_t = uint32_t;
+    using page_id_t = int32_t;
     using frame_id_t = size_t;
     using seq_t = uint64_t;
 
