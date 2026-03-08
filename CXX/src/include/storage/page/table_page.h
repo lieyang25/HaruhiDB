fdf41c911ca30881a3813633a3b4285780d3e1bc
suzumiya_haruhi <suzumiya@haruhi.com>
2026-03-08 17:01:05 +0800

    Update Slot and add function to TablePage

--- CXX/src/include/storage/page/table_page.h
+++ CXX/src/include/storage/page/table_page.h
@@ -1,6 +1,7 @@
 /**
  * CXX/src/include/storage/page/table_page.h
  */
+#include "record/tuple.h"
 #include "page.h"
 #include "common/config.h"
 
@@ -11,8 +12,20 @@ namespace storage
 {
     struct Slot
     {
-        uint16_t offest;
+        uint16_t offset;
         uint16_t length;
+
+        uint16_t GetOffset() const {return offset;}
+        uint16_t GetLength() const {return length & TUPLE_LENGTH;}
+
+        void SetOffset(uint16_t offset) {offset = offset;}
+        void SetLength(uint16_t length) {length |= length;}
+
+        void SetDeleted() {length |= TUPLE_FLAG_DEL;}
+        void SetMoved() {length |= TUPLE_FLAG_MOV;}
+        
+        bool IsDeleted() const {return (length & TUPLE_FLAG_DEL) != 0;}
+        bool IsMoved() const {return (length & TUPLE_FLAG_MOV) != 0;}
     };
     
     class TablePage
@@ -21,10 +34,11 @@ namespace storage
         explicit TablePage(Page* page):page_(page){}
         ~TablePage() = default;
 
-        void InsertTuple();
-        void UpdateTuple();
-        void DelTuple();
-        void GetTuple();
+        slot_id_t InsertTuple(record::Tuple& tuple);
+        bool UpdateTuple(slot_id_t slot_id,record::Tuple& tuple);
+        bool MarkDelTuple(slot_id_t slot_id);
+        bool GetTuple(slot_id_t slot_id,record::Tuple& tuple);
+
         Slot* SlotArray();
         const Slot* SlotArray() const;
         Slot* GetSlot(slot_id_t slot_id);
