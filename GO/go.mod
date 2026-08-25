2e105e2743905f2b41c0980498366a1238c25e7d
suzumiya_haruhi <suzumiya@haruhi.com>
2026-08-25 16:22:45 +0800

    chore: trim project to database kernel

--- GO/go.mod
+++
@@ -1,3 +0,0 @@
-module haruhidb-go
-
-go 1.20
--- GO/haruhidb/cgo_flags_unix.go
+++
@@ -1,9 +0,0 @@
-//go:build cgo && (linux || darwin)
-
-package haruhidb
-
-/*
-#cgo CFLAGS: -I${SRCDIR}/../../CXX/src/include
-#cgo LDFLAGS: -L${SRCDIR}/../../CXX/build/src/capi -lharuhidb_capi -Wl,-rpath,${SRCDIR}/../../CXX/build/src/capi
-*/
-import "C"
